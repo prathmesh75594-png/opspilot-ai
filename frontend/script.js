05f34e83184bd6a7d894b7d54b4f9978bbbc89fc
prathmesh75594-png <prathmesh75594@gmail.com>
2026-08-22 19:28:45 +0530

    Fix production API URL

--- frontend/script.js
+++ frontend/script.js
@@ -26,7 +26,7 @@ async function askAI() {
     try {
 
         const response = await fetch(
-            "http://localhost:5000/api/ask",
+            "/api/ask",
             {
                 method: "POST",
 
